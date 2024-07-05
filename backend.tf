b8b9a2459cf56f805e72e2ad53f8d0e863b1dab6
pavan pavi <pavanpavi56@gmail.com>
2024-07-05 17:26:42 +0530

    deleted extra space

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-bucket-terraform "
+    bucket = "my-aws-bucket-terraform"
     region = "ap-south-1"
     key = "jenkins-server/terraform.tfstate"
   }
