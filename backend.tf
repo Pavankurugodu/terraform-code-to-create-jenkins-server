eea91641255e1fded11211b3b86c8c5fd268536e
pavan pavi <pavanpavi56@gmail.com>
2024-07-05 17:23:29 +0530

    changed AZ

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "my-aws-bucket-terraform "
+    region = "ap-south-1"
     key = "jenkins-server/terraform.tfstate"
   }
 }
